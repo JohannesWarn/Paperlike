4b4aee5190c7abab485acc6c83bae995f9577b3a
Johannes Wärn <johannes@forzafootball.com>
2019-01-21 10:12:32 +0100

    Initial Commit

---
+++ Paperlike/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Paperlike
+//
+//  Created by Johannes @forza on 2019-01-21.
+//  Copyright © 2019 Johannes Wärn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Paperlike/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Paperlike
+//
+//  Created by Johannes @forza on 2019-01-21.
+//  Copyright © 2019 Johannes Wärn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
